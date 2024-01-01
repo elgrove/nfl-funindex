5d1f5248c92ce020b684865033b973e6c657d85f
Aaron <aaron0lovegrove@gmail.com>
2024-01-01 16:19:00 +0000

    deploy

--- README.md
+++ README.md
@@ -2,12 +2,15 @@
 
 A website that scrapes data from sources and rates past NFL games on how fun they were to watch.
 
-I use Cloudflare for DNS on my domain `lgrv.net` which does not play nicely with AWS CloudFront at all, so the site is
-only viewable at [d1m1ehvj01f3jr.cloudfront.net](https://d1m1ehvj01f3jr.cloudfront.net/) rather than something nicer.
+Ideally I would host this at `nfl.lgrv.net` as a subdomain of my personal domain, but I use Cloudflare for DNS which
+does not play nicely with AWS CloudFront at all, so the site is only viewable
+at [d1m1ehvj01f3jr.cloudfront.net](https://d1m1ehvj01f3jr.cloudfront.net/) rather than a pretty URL.
 
 This was previously a Flask app deployed on a VPS with Docker and Nginx, but I have recently rewritten it to use
 serverless, event-driven microservices on AWS, all defined as infrastructure-as-code, with automated, reproducible
 builds.
+
+![infrastructure diagram](./docs/nfl-serverless.drawio.png)
 
 ### How the build works
 
@@ -21,7 +24,11 @@ builds.
 
 ### How the app works
 
--
+- A Lambda runs every 2 hours checking the source for new games that don't have data in the DynamoDB table
+- If it finds games that require scraping, it will put them in an event on the default AWS EventBridge event bus
+- Another Lambda is listening to the event bus and will scrape the game(s) into the DynamoDB table
+- The DynamoDB table has streams enabled and will send events whenever the table changes
+- A third Lambda is list
 
 ### Installation
 
@@ -48,7 +55,7 @@ builds.
 - S3 for static web page storage and hosting
 - BeautifulSoup and Selenium for scraping
 - Pandas for turning HTML into dataframes
-- Jinja2 for website templates
+- Jinja2 for HTML templating
 
 ### License
 
--- infra/main.tf
+++ infra/main.tf
@@ -53,113 +53,111 @@ resource "aws_s3_bucket" "nfl-site" {
 }
 
 
-#
-#resource "aws_s3_bucket_public_access_block" "nfl-site" {
-#  bucket = aws_s3_bucket.nfl-site.id
-#
-#  block_public_acls       = true
-#  block_public_policy     = true
-#  ignore_public_acls      = true
-#  restrict_public_buckets = true
-#}
-#
-#resource "aws_s3_bucket_versioning" "nfl-site" {
-#  bucket = aws_s3_bucket.nfl-site.id
-#  versioning_configuration {
-#    status = "Enabled"
-#  }
-#}
-#
-#resource "aws_s3_bucket_ownership_controls" "ebab_website_ownership" {
-#  bucket = aws_s3_bucket.nfl-site.id
-#
-#  rule {
-#    object_ownership = "BucketOwnerEnforced"
-#  }
-#}
-#
-#resource "aws_s3_bucket_policy" "nfl-site" {
-#  bucket = aws_s3_bucket.nfl-site.id
-#  policy = data.aws_iam_policy_document.nfl-site.json
-#}
-#
-#
-#data "aws_iam_policy_document" "nfl-site" {
-#  statement {
-#    effect = "Allow"
-#
-#    actions = ["s3:GetObject*"]
-#
-#    resources = ["${aws_s3_bucket.nfl-site.arn}/*"]
-#
-#    principals {
-#      type        = "Service"
-#      identifiers = ["cloudfront.amazonaws.com"]
-#    }
-#
-#    condition {
-#      test   = "StringEquals"
-#      values = [
-#        "arn:aws:cloudfront::${data.aws_caller_identity.chalice.account_id}:distribution/${aws_cloudfront_distribution.nfl-site.id}"
-#      ]
-#      variable = "aws:SourceArn"
-#    }
-#  }
-#  # statement {
-#  #   sid       = "HTTPSOnly"
-#  #   effect    = "Deny"
-#  #   actions   = ["s3:*"]
-#  #   resources = [aws_s3_bucket.nfl-site.arn, "${aws_s3_bucket.nfl-site.arn}/*"]
-#  #   principals {
-#  #     identifiers = ["*"]
-#  #     type        = "AWS"
-#  #   }
-#  #   condition {
-#  #     test     = "Bool"
-#  #     values   = [false]
-#  #     variable = "aws:SecureTransport"
-#  #   }
-#  # }
-#}
-#
-#resource "aws_cloudfront_origin_access_control" "nfl-site" {
-#  name                              = "nfl-site"
-#  description                       = "Origin Access Control for the nfl-site CloudFront distribution"
-#  origin_access_control_origin_type = "s3"
-#  signing_behavior                  = "always"
-#  signing_protocol                  = "sigv4"
-#}
-#
-#resource "aws_cloudfront_distribution" "nfl-site" {
-#  origin {
-#    domain_name              = aws_s3_bucket.nfl-site.bucket_regional_domain_name
-#    origin_access_control_id = aws_cloudfront_origin_access_control.nfl-site.id
-#    origin_id                = "NflSiteOrigin"
-#  }
-#
-#  enabled             = true
-#  comment             = "nfl-site distribution"
-#  default_root_object = "current.html"
-#  aliases             = ["nfl.lgrv.net"]
-#
-#
-#  default_cache_behavior {
-#    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
-#    cached_methods         = ["GET", "HEAD"]
-#    target_origin_id       = "NflSiteOrigin"
-#    cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # Caching disabled
-#    viewer_protocol_policy = "redirect-to-https"
-#  }
-#
-#  viewer_certificate {
-#    cloudfront_default_certificate = true
-#  }
-#
-#  restrictions {
-#    geo_restriction {
-#      locations        = []
-#      restriction_type = "none"
-#    }
-#  }
-#
-#}
+resource "aws_s3_bucket_public_access_block" "nfl-site" {
+  bucket = aws_s3_bucket.nfl-site.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "nfl-site" {
+  bucket = aws_s3_bucket.nfl-site.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "ebab_website_ownership" {
+  bucket = aws_s3_bucket.nfl-site.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_policy" "nfl-site" {
+  bucket = aws_s3_bucket.nfl-site.id
+  policy = data.aws_iam_policy_document.nfl-site.json
+}
+
+
+data "aws_iam_policy_document" "nfl-site" {
+  statement {
+    effect = "Allow"
+
+    actions = ["s3:GetObject*"]
+
+    resources = ["${aws_s3_bucket.nfl-site.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test   = "StringEquals"
+      values = [
+        "arn:aws:cloudfront::${data.aws_caller_identity.chalice.account_id}:distribution/${aws_cloudfront_distribution.nfl-site.id}"
+      ]
+      variable = "aws:SourceArn"
+    }
+  }
+  # statement {
+  #   sid       = "HTTPSOnly"
+  #   effect    = "Deny"
+  #   actions   = ["s3:*"]
+  #   resources = [aws_s3_bucket.nfl-site.arn, "${aws_s3_bucket.nfl-site.arn}/*"]
+  #   principals {
+  #     identifiers = ["*"]
+  #     type        = "AWS"
+  #   }
+  #   condition {
+  #     test     = "Bool"
+  #     values   = [false]
+  #     variable = "aws:SecureTransport"
+  #   }
+  # }
+}
+
+resource "aws_cloudfront_origin_access_control" "nfl-site" {
+  name                              = "nfl-site"
+  description                       = "Origin Access Control for the nfl-site CloudFront distribution"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "nfl-site" {
+  origin {
+    domain_name              = aws_s3_bucket.nfl-site.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.nfl-site.id
+    origin_id                = "NflSiteOrigin"
+  }
+
+  enabled             = true
+  comment             = "nfl-site distribution"
+  default_root_object = "current.html"
+
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "NflSiteOrigin"
+    cache_policy_id        = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" # Caching disabled
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+
+}
